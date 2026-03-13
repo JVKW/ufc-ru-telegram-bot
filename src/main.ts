0c32454edbe1b8642d9a311245ad93cb706d8819
JVKW <joaovictor.os064@gmail.com>
2026-03-13 15:39:18 -0300

    refactor: migrate bot initialization to grammY

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
-import { bot } from './bot/bot.js'
-import { registrarComandos } from './bot/registerCommands.js'
+import { bot } from "./bot/bot.js"
+import { registrarComandos } from "./bot/registerCommands.js"
 
 async function start() {
-    await registrarComandos(bot)
-    console.log('🤖 Bot rodando...')
+
+  await registrarComandos(bot)
+
+  console.log("🤖 Bot rodando...")
+
+  bot.start()
+
 }
 
 start()
